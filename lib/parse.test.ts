1a730ecdb7cb080297a283ff29e7c04d816da619
Gustaf Räntilä <g.rantila@gmail.com>
2022-04-20 18:47:39 +0200

    feat(undefined): add support for handling undefined input by mimicing null behavior

--- lib/parse.test.ts
+++ lib/parse.test.ts
@@ -28,6 +28,56 @@ describe( "parse", ( ) =>
 		);
 	} );
 
+	it( "getAstByObject null", ( ) =>
+	{
+		const { json, jsonString, jsonAST } = getAstByObject( null );
+		expect( json ).toBe( null );
+		expect( jsonString ).toBe( 'null' );
+		expect( jsonAST ).toMatchObject( {
+			loc: {
+				end: {
+					column: 5,
+					line: 1,
+					offset: 4,
+				},
+				source: null,
+				start: {
+					column: 1,
+					line: 1,
+					offset: 0,
+				},
+			},
+			raw: 'null',
+			type: 'Literal',
+			value: null,
+		} );
+	} );
+
+	it( "getAstByObject undefined", ( ) =>
+	{
+		const { json, jsonString, jsonAST } = getAstByObject( undefined );
+		expect( json ).toBe( undefined );
+		expect( jsonString ).toBe( 'undefined' );
+		expect( jsonAST ).toMatchObject( {
+			loc: {
+				end: {
+					column: 10,
+					line: 1,
+					offset: 9,
+				},
+				source: undefined,
+				start: {
+					column: 1,
+					line: 1,
+					offset: 0,
+				},
+			},
+			raw: 'undefined',
+			type: 'Literal',
+			value: undefined,
+		} );
+	} );
+
 	it( "getAstByObject with custom indent", ( ) =>
 	{
 		const str = '{\n  "foo": {\n    "bar": "baz"\n  }\n}';
--- lib/parse.ts
+++ lib/parse.ts
@@ -1,4 +1,4 @@
-import parse from 'json-to-ast'
+import parse, { type LiteralNode } from 'json-to-ast'
 
 
 export interface ParsedJson
@@ -20,6 +20,25 @@ export function getAstByString( jsonString: string, json?: any ): ParsedJson
 
 export function getAstByObject( json: any, indent = 4 ): ParsedJson
 {
-	const jsonString = JSON.stringify( json, null, indent );
-	return getAstByString( jsonString );
+	const jsonString = JSON.stringify( json ?? null, null, indent );
+	const ret = getAstByString( jsonString );
+
+	// When we get undefined as input, mimic null behavior
+	if ( json === undefined )
+	{
+		const jsonAST = ret.jsonAST as LiteralNode;
+		// istanbul ignore next
+		if ( jsonAST.loc?.end )
+		{
+			jsonAST.loc.end.column += 5;
+			jsonAST.loc.end.offset += 5;
+			jsonAST.loc.source = undefined as any as null;
+		}
+		jsonAST.value = undefined as any as null;
+		jsonAST.raw = 'undefined';
+		ret.jsonString = 'undefined';
+		ret.json = undefined;
+	}
+
+	return ret;
 }
